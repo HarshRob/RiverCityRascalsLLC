27fe11b1041eadaef4e4ebe6db4b8bbe4e8bc153
Robbie Harshman <robbie.harshman@gmail.com>
2021-04-15 11:49:58 -0700

    Working Tabs

--- js/index.js
+++ js/index.js
@@ -1,39 +1,40 @@
 const _aboutUs = document.getElementById("aboutus");
-const _eventCalendar = document.querySelector("#eventcalendar");
+const _eventCalendar = document.getElementById("eventcalendar");
 const _pricingAvailability = document.getElementById("pricingavailability");
+const _aboutUsTab = document.getElementById("aboutUsTab");
+const _eventCalendarTab = document.getElementById("eventCalendarTab");
+const _pricingAvailabilityTab = document.getElementById("pricingAvailabilityTab");
 
-console.log(_aboutUs);
 //onclick functions to open the respective tabs
 function aboutUs() {
-    if(_aboutUs.style.display === "none") {
+    if(_aboutUs.style.display === "none" || _aboutUs.style.display === "") {
         _aboutUs.style.display = "block";
         _eventCalendar.style.display = "none";
         _pricingAvailability.style.display = "none";
-        _aboutUs.classList.add('active')
-        _eventCalendar.classList.remove('active')
-        _pricingAvailability.classList.remove('active')
-
+        _aboutUsTab.classList.add('active')
+        _eventCalendarTab.classList.remove('active')
+        _pricingAvailabilityTab.classList.remove('active')
     }
 }
 
 function eventCalendar() {
-    if(_eventCalendar.style.display === "none") {
+    if(_eventCalendar.style.display === "none" || _eventCalendar.style.display === "") {
         _aboutUs.style.display = "none";
         _eventCalendar.style.display = "block";
         _pricingAvailability.style.display = "none";
-        _aboutUs.classList.remove('active')
-        _eventCalendar.classList.add('active')
-        _pricingAvailability.classList.remove('active')
+        _aboutUsTab.classList.remove('active')
+        _eventCalendarTab.classList.add('active')
+        _pricingAvailabilityTab.classList.remove('active')
     }
 }
 
 function pricingAvailability() {
-    if(_pricingAvailability.style.display === "none") {
+    if(_pricingAvailability.style.display === "none" || _pricingAvailability.style.display === "") {
         _aboutUs.style.display = "none";
         _eventCalendar.style.display = "none";
         _pricingAvailability.style.display = "block";
-        _aboutUs.classList.remove('active')
-        _eventCalendar.classList.remove('active')
-        _pricingAvailability.classList.add('active')
+        _aboutUsTab.classList.remove('active')
+        _eventCalendarTab.classList.remove('active')
+        _pricingAvailabilityTab.classList.add('active')
     }
 }
